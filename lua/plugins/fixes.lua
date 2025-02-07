3ddcbcea544631c08da323b11b866f37bd7c366c
Marla Albuquerque <marla@albuque.com>
2025-02-07 15:27:05 -0300

    fix(nvim-ufo): waiting for merge

---
+++ lua/plugins/fixes.lua
@@ -0,0 +1,9 @@
+-- Fixes missing a pull request to be merged or to be fixed by its developers.
+
+return {
+  -- https://github.com/kevinhwang91/nvim-ufo/issues/254
+  {
+    "kevinhwang91/nvim-ufo",
+    version = false,
+  },
+}
